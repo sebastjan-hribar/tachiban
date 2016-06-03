68764bc01cb4bd7bddc02cdf88fc8fa09396d2d8
Vasilis Spilka <vasspilka@gmail.com>
2016-06-03 16:39:09 +0300

    split tachiban test

--- test/tachiban_test.rb
+++ test/tachiban_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
+include Hanami::Tachiban
 
-class TachibanTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Tachiban::VERSION
+describe 'Hanami::Tachiban' do
+
+  it 'can generate password' do
+    password = hashed_password("pass123")
+    password.must_be_kind_of String
   end
+
+  it "password_reset_sent_at is of type Time" do
+    password_reset_sent_at.must_be_kind_of Time
+  end
+
+  it "generates unique token" do
+    token.length > 20
+  end
+
 end
